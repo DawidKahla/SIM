a634018c2d3afe5bca57f913666082207b29d5a6
DawidKahla <51680032+DawidKahla@users.noreply.github.com>
2020-04-17 15:28:38 +0200

    Update GAME_Ship.h

--- Inc/GAME_Ship.h
+++ Inc/GAME_Ship.h
@@ -9,7 +9,7 @@
 //#define GAME_SHIP_H_
 //
 //#include "PAW_Figure.h"
-///* pocz¹tkowy pomys³ utworzenia nowej struktury okaza³ siê ma³o wydajny */
+///* poczÂ¹tkowy pomysÂ³ utworzenia nowej struktury okazaÂ³ siÃª maÂ³o wydajny */
 ////typedef struct {
 ////	PAW_Figure fig;
 ////	uint16_t scale;
@@ -22,10 +22,5 @@
 ////void GAME_Ship_draw(GAME_Ship* const ship, const uint32_t a_color);
 ////void GAME_Ship_show(GAME_Ship* const ship, const uint32_t buffer);
 //
-///* zdecydowa³em sie traktowac statek jako figure */
-//
-//
-//
-//
-//
+///* zdecydowaÂ³em sie traktowac statek jako figure */
 //#endif /* GAME_SHIP_H_ */
